db99fbdd98c4b522d8b3867eb159e7e7913c7dc2
mattia <mattiav.dev@gmail.com>
2022-06-16 22:05:25 +0300

    · added redux-persist to use Local Storgae to store the global state (redux store)

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,25 @@
 import {compose, applyMiddleware} from 'redux';
-import { legacy_createStore as createStore} from 'redux'
+import { legacy_createStore as createStore} from 'redux';
+
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import {rootReducer} from './root-reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [logger];
 
 const composeEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
