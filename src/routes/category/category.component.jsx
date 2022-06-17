131dbd287c69fbdffc1328169769e2d3f153c72d
mattia <mattiav.dev@gmail.com>
2022-06-17 22:39:00 +0300

    · added redux-thunk to handle async requests with isLading state

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,13 +3,15 @@ import {useParams} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
 
 import {CategoryContainer, CategoryTitle} from './category.styles';
 
 const Category = () => {
   const {category} = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -19,14 +21,15 @@ const Category = () => {
   return(
     <Fragment>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {
-          //need to perform a check to avoid issue with the async request of data from Firestore
-
-          //Here checking if "products" contains the data. If not, not rendering (ideally a spinner/loader)
-          products && products.map(product => <ProductCard key={product.id} product={product} />)
-        }
-      </CategoryContainer>
+      {
+        isLoading 
+        ? (<Spinner />)
+        : (<CategoryContainer>
+          {            
+            products && products.map(product => <ProductCard key={product.id} product={product} />)
+          }
+        </CategoryContainer>)
+      }
     </Fragment>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -3,20 +3,23 @@ import { legacy_createStore as createStore} from 'redux';
 
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import {rootReducer} from './root-reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger, 
+  thunk
+].filter(Boolean);
 
 const composeEnhancers = compose(applyMiddleware(...middleWares))
 
